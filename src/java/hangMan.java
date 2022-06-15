c62734524409fc701898e63c738b824c82cc8c0d
Joel Brinson <jtbrinson11@gmail.com>
2022-06-15 16:25:35 -0400

    Basic program written

--- src/java/hangMan.java
+++ src/java/hangMan.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class hangMan {
 
     static Scanner scan = new Scanner(System.in);
-    static String[] words = {"Bob", "Vectrex", "Luke", "Iron", "Rhododendron", "Stearman", "Eagle", "Taco", "Pizza", "Capacitor", "Scion", "Blaze", "Galaga", "Boeing", "Konami"};
+    static String[] words = {"Joel", "Vectrex", "Luke", "Iron", "Rhododendron", "Stearman", "Eagle", "Taco", "Pizza", "Capacitor", "Blaze", "Scion", "Galaga", "Boeing", "Konami"};
+    static String[] wrong = {"\n+----------+ \n" + "           | \n" + "           | \n" + "           | \n" + "            === \n", "\n+----------+ \n" + " O         | \n" + "           | \n" + "           | \n" + "            === \n", "\n+----------+ \n" + " O         | \n" + " |         | \n" + "           | \n" + "            === \n", "\n+----------+ \n" + " O         | \n" + " |         | \n" + " |         | \n" + "            === \n", "\n+----------+ \n" + " O         | \n" + " |         | \n" + " |         | \n" + "/           === \n", "\n+----------+ \n" + " O         | \n" + " |         | \n" + " |         | \n" + "/ \\         === \n", "\n+----------+ \n" + " O         | \n" + "/|         | \n" + " |         | \n" + "/ \\         === \n", "\n+----------+ \n" + " O         | \n" + "/|\\        | \n" + " |         | \n" + "/ \\         === \n"};
+    static String[] word;
     static String userInput = "";
+    static String wordPick = "";
+
+    static String missedLetters = "";
+    static int wordLen;
     static int wordUse = 0;
-    static int tries = 0;
+    static int numWrong;
+    static boolean done = false;
+    static boolean complete = false;
 
 
     static void setInput()
@@ -20,26 +28,157 @@ public class hangMan {
         return userInput;
     }
 
+    static void computeOutcome(String input)
+    {
+        input = input.toLowerCase();
+        char let = input.charAt(0);
+        wordPick = wordPick.toLowerCase();
+        int letNum = wordPick.indexOf(input);
+
+        boolean found = false;
+        if (letNum != -1)
+        {
+            found = true;
+        }
+
+        if (found == false)
+        {
+            System.out.println(wrong[++numWrong]);
+            missedLetters += " " + input.toUpperCase();
+            System.out.println(missedLetters);
+            for (int i = 0; i < word.length; i++)
+            {
+                System.out.print(word[i] + " ");
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wordLen; i++)
+            {
+                if (wordPick.charAt(i) == let)
+                {
+                    word[i] = input.toUpperCase();
+                }
+            }
+            System.out.println(wrong[numWrong]);
+            System.out.println(missedLetters);
+            for (int i = 0; i < word.length; i++)
+            {
+                System.out.print(word[i] + " ");
+            }
+        }
+
+        char done = ' ';
+        for (int i = 0; i < word.length; i++)
+        {
+            String letter = word[i];
+            char letCur = letter.charAt(0);
+            if (done == letCur)
+            {
+                complete = false;
+                break;
+            }
+            else
+            {
+                complete = true;
+            }
+        }
+    }
+
     static void playGame()
     {
+        numWrong = 0;
         wordUse = (int)(Math.random() * (15 - 1) - 1);
-        boolean done = false;
-        while(!done)
+        wordLen = words[wordUse].length();
+        wordPick = words[wordUse];
+        word = new String[wordLen];
+        missedLetters = "Missed letters: ";
+        done = false;
+        complete = false;
+        System.out.println("\nH A N G M A N \n");
+        System.out.println(wrong[0]);
+        System.out.println(missedLetters);
+
+        for (int i = 0; i < word.length; i++)
         {
-            System.out.println("H A N G M A N \n" + "\n" + "+----------+ \n" + "           | \n" + "           | \n" + "           | \n" + "            === \n" + "     Missed letters: \n");
-            for (int i = 0; i < words[wordUse].length(); i++)
+            word[i] = " _ ";
+        }
+        for (int i = 0; i < word.length; i++)
+        {
+            System.out.print(word[i]);
+        }
+        while(numWrong != 7)
+        {
+
+            if (!complete)
             {
-                System.out.print("_ ");
+                setInput();
+                String guess = getInput();
+                computeOutcome(guess);
+            }
+            else
+            {
+                break;
             }
 
-            setInput();
 
         }
     }
 
     public static void main(String[] args)
     {
-        playGame();
+        while(!done)
+        {
+            playGame();
+
+            if (complete)
+            {
+                System.out.println("\n\nCongratulations, you guessed my word!");
+                System.out.println("Do you want to play again (y/n): ");
+                userInput = scan.nextLine();
+                switch (userInput)
+                {
+                    case "Y":
+                        System.out.println("Ok, I'll reset the game.");
+                        break;
+                    case "y":
+                        System.out.println("Resetting the game, then.");
+                        break;
+                    case "N":
+                        System.out.println("Ok, good-bye for now.");
+                        done = true;
+                        break;
+                    case "n":
+                        System.out.println("Good-bye for now.");
+                        done = true;
+                        break;
+                }
+            }
+            else if (numWrong == 7)
+            {
+                System.out.println("\n\nSorry, you ran out of guesses.");
+                System.out.println("The word was: " + wordPick + ".");
+                System.out.println("Do you want to play again (y/n): ");
+                userInput = scan.nextLine();
+                switch (userInput)
+                {
+                    case "Y":
+                        System.out.println("Ok, I'll reset the game.");
+                        break;
+                    case "y":
+                        System.out.println("Resetting the game, then.");
+                        break;
+                    case "N":
+                        System.out.println("Ok, good-bye for now.");
+                        done = true;
+                        break;
+                    case "n":
+                        System.out.println("Good-bye for now.");
+                        done = true;
+                        break;
+                }
+            }
+        }
+
     }
 }
-
